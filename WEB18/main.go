3586b48f57c99d6f5ab5e28d784b7df4be004802
Jungheon-Oh <zsnd.oj@gmail.com>
2021-09-21 22:46:10 +0900

    todo

---
+++ WEB18/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"GoBlock/goweb/WEB18/app"
+
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	m := app.MakeHandler()
+	n := negroni.Classic()
+	n.UseHandler(m)
+
+	log.Println("Started App")
+	err := http.ListenAndServe(":3000", n)
+	if err != nil {
+		panic(err)
+	}
+}
