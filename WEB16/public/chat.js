2cbfca2cece51f6b4a24996e695021d1ec66a12f
Jungheon-Oh <zsnd.oj@gmail.com>
2021-09-24 16:29:27 +0900

    chat

--- WEB16/public/chat.js
+++ WEB16/public/chat.js
@@ -25,6 +25,35 @@ $(function(){
         });
         $chatmsg.val("");
         $chatmsg.focus();
+        return false;
     });
+
+    var addMessage = function(data) {
+        var text = "";
+        if (!isBlank(data.name)) {
+            text = '<strong>' + data.name + ':</strong> ';
+        }
+        text += data.msg;
+        $chatlog.prepend('<div><span>' + text + '</span></div>');
+    };
+
+    var es = new EventSource('/stream');
+    es.onopen = function(e) {
+        $.post('users/', {
+            name: username
+        });
+    }
+    es.onmessage = function(e) {
+        var msg = JSON.parse(e.data);
+        addMessage(msg);
+    };
+
+    window.onbeforeunload = function() {
+        $.ajax({
+            url: "/users?username=" + username,
+            type: "DELETE"
+        });
+        es.close()
+    };
 
 })
